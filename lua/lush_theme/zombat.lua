a87d4f7199d05bf0dee572aaffffcfe469353d30
Russ Buchanan <rbuchss@gmail.com>
2025-10-05 22:00:43 -0400

    update search color (#5)

--- lua/lush_theme/zombat.lua
+++ lua/lush_theme/zombat.lua
@@ -111,7 +111,7 @@ local theme = lush(function(injected_functions)
     -- PmenuThumb     { }, -- Popup menu: Thumb of the scrollbar.
     -- Question       { }, -- |hit-enter| prompt and yes/no questions
     -- QuickFixLine   { }, -- Current |quickfix| item in the quickfix window. Combined with |hl-CursorLine| when the cursor is there.
-    Search            { fg = hsl('#ffff87'), bg = hsl('#0087ff'), }, -- Last search pattern highlighting (see 'hlsearch'). Also used for similar items that need to stand out.
+    Search            { fg = hsl('#ffff87'), bg = hsl('#73186e'), }, -- Last search pattern highlighting (see 'hlsearch'). Also used for similar items that need to stand out.
     SpecialKey        { fg = hsl('#626262'), bg = hsl('#2b2b2b'), }, -- Unprintable characters: text displayed differently from what it really is. But not 'listchars' whitespace. |hl-Whitespace|
     SpellBad          { }, -- Word that is not recognized by the spellchecker. |spell| Combined with the highlighting used otherwise.
     -- SpellCap       { }, -- Word that should start with a capital. |spell| Combined with the highlighting used otherwise.
